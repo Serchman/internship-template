4201a0ce93aeb09c2802cf9afb6f9e65dadd1d83
Serchman <polishmetal@gmail.com>
2017-04-10 22:11:50 +0200

    Update tasks.js

--- internship/tasks.js
+++ internship/tasks.js
@@ -1,13 +1,21 @@
-
-
-//var div = ('#task1')
-
-calculateAge = function(b_year,c_year) {
-        var poss_age = c_year - b_year;
-        var poss_age2 = parseInt(poss_age) +1
-        var output = "You are either " + poss_age + " or " + poss_age2
+calculateAge = function(b_year) {
+  
+        var today = new Date();
+        var year =  today.getFullYear();
+        var poss_age = year - b_year;
+        var poss_age2 = parseInt(poss_age) -1
+        console.log(year)
+        
+        var output = "You are either " + poss_age2 + " or " + poss_age
         console.log("You are either " + poss_age + " or " + poss_age2);
+  
+        
         document.getElementById('task1').innerHTML = output
 };
 
-calculateAge(1992,2017);
+function handleClick(event){
+                var x = document.getElementById("myVal1").value;
+	    	console.log(x)
+          calculateAge(x)
+                return false;
+            }
