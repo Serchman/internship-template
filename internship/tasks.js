03064f0bd8cae3b69b749f37365cb602b5f7799e
Serchman <polishmetal@gmail.com>
2017-04-10 19:23:37 +0200

    Update tasks.js

--- internship/tasks.js
+++ internship/tasks.js
@@ -7,7 +7,7 @@ calculateAge = function(b_year,c_year) {
         var poss_age2 = parseInt(poss_age) +1
         var output = "You are either" + poss_age + " or " + poss_age2
         console.log("You are either" + poss_age + " or " + poss_age2);
-        document.getElementById(task1).innerHTML = (output)
+        document.getElementById(task1).innerHTML =+ output
 };
 
 calculateAge(1992,2017);
