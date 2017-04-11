5e41c7f8a6b3e550d13e6e424a29d0a40bd21146
Serchman <polishmetal@gmail.com>
2017-04-11 11:04:52 +0200

    Update tasks.js

--- internship/tasks.js
+++ internship/tasks.js
@@ -6,7 +6,7 @@ calculateAge = function(b_year) {
         var poss_age2 = parseInt(poss_age) -1
         console.log(year)
         
-        var output = "You are either " + poss_age2 + " or " + poss_age
+        var output = "You are either " + poss_age2 + " or " + poss_age + "."
         console.log("You are either " + poss_age + " or " + poss_age2);
   
         
@@ -30,3 +30,52 @@ calculateSupply = function (age,amount){
 }
 
 calculateSupply(25,3)
+
+
+
+calcCircumference = function(radius){
+  var circum = radius * 2 * Math.PI
+  
+  document.getElementById('circum').innerHTML = "The circumference is " + circum + ".";
+  
+}
+
+//calcCircumference(4)
+
+calcArea = function(radius){
+  var area = radius * radius * Math.PI;
+  
+  document.getElementById('area').innerHTML = 'The area is ' + area + ".";
+  
+}
+
+//calcArea(4)
+
+function handleClick(event){
+                var x = document.getElementById("radius").value;
+	    	console.log(x)
+          calcCircumference(x)
+          calcArea(x)
+                return false;
+            }
+
+
+
+celsiusToFahrenheit = function(celcius){
+  
+  var C = parseInt(celcius)
+  var F = (C *9) / 5 + 32
+  document.getElementById('faren').innerHTML = C + String.fromCharCode(176) + "C is " + F + String.fromCharCode(176) + "F."
+}
+
+celsiusToFahrenheit(0)
+
+
+fahrenheitToCelsius = function(fahrenheit){
+  
+  var F = parseInt(fahrenheit)
+  var C = ((F - 32) * 5) /9
+  document.getElementById('celc').innerHTML = F + String.fromCharCode(176) + "F is " + C + String.fromCharCode(176) + "C."
+}
+
+fahrenheitToCelsius(100)
